29425bdef22db03c3b83c5aa3023d4634dba9826
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-06-02 05:33:18 -0300

    Criando JPnale, JTable e JScrollPane para o JFrame.

--- src/main/java/exemplo01/tabela/TabelaSimples.java
+++ src/main/java/exemplo01/tabela/TabelaSimples.java
@@ -1,8 +1,18 @@
 package exemplo01.tabela;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 /**
+ * By: Jhonatan Mattana
  * 2 de jun de 2017 - 05:25:55 
  */
-public class TabelaSimples {
+public class TabelaSimples extends JFrame{
+	JPanel painelFundo;
+	JTable tabela;
+	JScrollPane barraDeRolagem;
+	
 	
 }
