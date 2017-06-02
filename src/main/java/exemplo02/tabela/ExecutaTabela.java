2b3c54782ef723f9269951fbeaab5f14b2aa423a
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-06-02 06:06:13 -0300

    Dando inicio ao segundo Exemplo de tabelas

---
+++ src/main/java/exemplo02/tabela/ExecutaTabela.java
@@ -0,0 +1,46 @@
+package exemplo02.tabela;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * By: Jhonatan Mattana
+ * 2 de jun de 2017 - 06:04:05 
+ */
+public class ExecutaTabela extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ExecutaTabela frame = new ExecutaTabela();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ExecutaTabela() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
