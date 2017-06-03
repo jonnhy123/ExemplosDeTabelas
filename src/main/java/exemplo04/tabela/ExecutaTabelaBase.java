355e2a6474bfc3e53ee2e3fd3bb6bcb377ce3c27
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-06-02 21:21:44 -0300

    Iniciando exemplo04 de tabelas

---
+++ src/main/java/exemplo04/tabela/ExecutaTabelaBase.java
@@ -0,0 +1,53 @@
+package exemplo04.tabela;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
+
+/**
+ * By: Jhonatan Mattana
+ * 2 de jun de 2017 - 21:19:50 
+ */
+public class ExecutaTabelaBase extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ExecutaTabelaBase frame = new ExecutaTabelaBase();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ExecutaTabelaBase() {
+		setTitle("Exemplo04 de tabelas");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0};
+		gbl_contentPane.rowHeights = new int[]{0};
+		gbl_contentPane.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+	}
+
+}
