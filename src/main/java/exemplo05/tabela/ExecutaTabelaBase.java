1526018c4621b88f020aa5e1a1e913ca2a8d40a0
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-06-05 04:20:19 -0300

    Iniciando exemplo05 de tabelas com conexão com de banco POSTGRES

---
+++ src/main/java/exemplo05/tabela/ExecutaTabelaBase.java
@@ -0,0 +1,46 @@
+package exemplo05.tabela;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * By: Jhonatan Mattana
+ * 5 de jun de 2017 - 04:19:30 
+ */
+public class ExecutaTabelaBase extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ExecutaTabelaBase frame = new ExecutaTabelaBase();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ExecutaTabelaBase() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
